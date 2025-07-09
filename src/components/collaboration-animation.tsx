9e2f9901d4bba14f43ba330678a7b15ec76696c8
Siddesh B <siddeshs532@gmail.com>
2025-07-09 06:12:21 +0000

    thats greate but make it 4d tesseract shape

--- src/components/collaboration-animation.tsx
+++ src/components/collaboration-animation.tsx
@@ -41,41 +41,79 @@ const CollaborationAnimation = ({ className }: { className?: string }) => {
             >
                 <style>
                 {`
-                    .cube-face {
+                    .tesseract-face {
                         position: absolute;
-                        width: 200px;
-                        height: 200px;
+                        left: 50%;
+                        top: 50%;
                         border: 1px solid hsl(var(--primary));
                         background: hsla(var(--primary) / 0.1);
                         box-shadow: 0 0 20px hsla(var(--primary) / 0.3), inset 0 0 20px hsla(var(--primary) / 0.2);
                         transition: all 6s ease-in-out;
                     }
-                    .morph-cube:hover .cube-face {
+                    .tesseract-container:hover .tesseract-face {
                         border-color: hsl(var(--accent));
                         background: hsla(var(--accent) / 0.15);
                         box-shadow: 0 0 35px hsla(var(--accent) / 0.5), inset 0 0 35px hsla(var(--accent) / 0.3);
                     }
-                    .morph-cube {
+                    .tesseract-container {
                         position: absolute;
                         width: 100%;
                         height: 100%;
                         transform-style: preserve-3d;
-                        animation: morph 12s infinite ease-in-out alternate;
+                        animation: tesseract-rotate 16s infinite linear;
                     }
-                    @keyframes morph {
-                        0% { transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1); }
-                        50% { transform: rotateX(180deg) rotateY(90deg) rotateZ(40deg) scale3d(0.8, 0.8, 0.8); }
-                        100% { transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg) scale3d(1, 1, 1); }
+
+                    @keyframes tesseract-rotate {
+                        0% { transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg); }
+                        100% { transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg); }
+                    }
+
+                    .inner-cube {
+                        transform-style: preserve-3d;
+                        animation: inner-cube-rotate 16s infinite linear reverse;
+                    }
+
+                    @keyframes inner-cube-rotate {
+                        0% { transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(0.5); }
+                        50% { transform: rotateX(-180deg) rotateY(-360deg) rotateZ(-180deg) scale(0.5); }
+                        100% { transform: rotateX(-360deg) rotateY(-720deg) rotateZ(-360deg) scale(0.5); }
+                    }
+
+                    .outer-cube {
+                        transform-style: preserve-3d;
+                    }
+
+                    .outer-cube .tesseract-face {
+                        width: 200px;
+                        height: 200px;
+                        margin-left: -100px;
+                        margin-top: -100px;
+                    }
+                    .inner-cube .tesseract-face {
+                        width: 100px;
+                        height: 100px;
+                        margin-left: -50px;
+                        margin-top: -50px;
                     }
                 `}
                 </style>
-                <div className="morph-cube">
-                    <div className="cube-face" style={{ transform: 'rotateY(0deg) translateZ(100px)' }}></div>
-                    <div className="cube-face" style={{ transform: 'rotateY(90deg) translateZ(100px)' }}></div>
-                    <div className="cube-face" style={{ transform: 'rotateY(180deg) translateZ(100px)' }}></div>
-                    <div className="cube-face" style={{ transform: 'rotateY(-90deg) translateZ(100px)' }}></div>
-                    <div className="cube-face" style={{ transform: 'rotateX(90deg) translateZ(100px)' }}></div>
-                    <div className="cube-face" style={{ transform: 'rotateX(-90deg) translateZ(100px)' }}></div>
+                <div className="tesseract-container">
+                    <div className="outer-cube">
+                        <div className="tesseract-face" style={{ transform: 'rotateY(0deg) translateZ(100px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(90deg) translateZ(100px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(180deg) translateZ(100px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(-90deg) translateZ(100px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateX(90deg) translateZ(100px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateX(-90deg) translateZ(100px)' }}></div>
+                    </div>
+                    <div className="inner-cube">
+                        <div className="tesseract-face" style={{ transform: 'rotateY(0deg) translateZ(50px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(90deg) translateZ(50px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(180deg) translateZ(50px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateY(-90deg) translateZ(50px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateX(90deg) translateZ(50px)' }}></div>
+                        <div className="tesseract-face" style={{ transform: 'rotateX(-90deg) translateZ(50px)' }}></div>
+                    </div>
                 </div>
             </div>
         </div>
